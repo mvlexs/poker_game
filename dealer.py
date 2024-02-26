62753490911460d0e409d5aa4962a7e1709c3fcb
mvlexs <ma.jak.lange@gmail.com>
2024-02-26 18:30:49 +0100

    .

--- dealer.py
+++ dealer.py
@@ -11,16 +11,13 @@ class Dealer ():
     centercard_5 = ""
     small_blind = 2
     big_blind = small_blind * 2
-    deck = ["♤A","♤2","♤3","♤4","♤5","♤6","♤7","♤8","♤9","♤10","♤J","♤Q","♤K"]
-    shuffled_deck = list
+
 
     def __init__(self, pot, centercard_1, centercard_2, centercard_3, centercard_4, centercard_5 , big_blind, small_blind, shuffled_deck, deck):
-        self.deck = deck
-        self.shuffled_deck = shuffled_deck
+        self.deck =  ["♤A","♤2","♤3","♤4","♤5","♤6","♤7","♤8","♤9","♤10","♤J","♤Q","♤K"]
+        self.shuffled_deck 
 
-    def shuffle_cards(self):
-        self.shuffled_deck = shuffle(self.deck)
-        return self.shuffled_deck
+   
 
         
         
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-from dealer import Dealer
+import deck
+import card
 
-this_dealer = Dealer
-print (this_dealer.shuffle_cards(this_dealer))
+x = card.Card("","")
+x.color = x.get_color
+x.value = x.get_value
+x.__repr__()
+print(x)
+new_deck = deck.Deck([],[])
+new_deck.fill_deck()
+'''test = new_deck.make_readable(new_deck.cards)
+print(test)'''
+print(new_deck.cards)
+
 
