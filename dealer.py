f8ed175a63016471e318beb44438163efb89d5cc
mvlexs <ma.jak.lange@gmail.com>
2024-02-28 18:50:00 +0100

    .

--- dealer.py
+++ dealer.py
@@ -17,8 +17,7 @@ class Dealer ():
     def __init__(self):#, pot, centercard_1, centercard_2, centercard_3, centercard_4, centercard_5 , big_blind, small_blind, shuffled_deck, deck):
         pass
 
-    def get_deck(self):
-        return self.deck.cards
+    
         
 
    
